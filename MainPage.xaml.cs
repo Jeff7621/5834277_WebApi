01b7f26346fc910d95c2aeb15017a494a0cf8a0f
Jefferson González <jefferg675@gmail.com>
2024-08-28 20:03:29 -0600

    Agregar archivos de proyecto.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using WebApi.Servicios;
+
+namespace WebApi
+{
+    public partial class MainPage : ContentPage
+    {
+        private readonly IRickAndMortyServices _rickAndMortyServices;
+
+
+
+        public MainPage(IRickAndMortyServices services)
+        {
+            InitializeComponent();
+            _rickAndMortyServices = services;
+        }
+         
+        private async void OnCounterClicked(object sender, EventArgs e)
+        {
+             Loading.IsVisible = true;
+            var data = await _rickAndMortyServices.Obtener();
+            listviewPersonajes.ItemsSource = data;
+
+            Loading.IsVisible = false;
+        }
+    }
+
+}
